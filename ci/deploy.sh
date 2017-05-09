4618ec4038a46696df0a6a4959e697a8c68e6c41
Barry Hoggard <barry@tristanmedia.com>
2017-05-08 22:22:37 -0400

    do a snapshot release for the development branch

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# do snapshot, no release
+if [ "$TRAVIS_BRANCH" = 'development' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_SCALA_VERSION" == "2.11.11" ]; then
+  sbt publishSigned
+end
+
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_SCALA_VERSION" == "2.11.11" ]; then
   sbt publishSigned
   sbt sonatypeRelease

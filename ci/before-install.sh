59fd224acfe51fa1e555f2193442d780724e45c1
Barry Hoggard <barry@tristanmedia.com>
2017-05-09 00:33:46 -0400

    fix gpg import

--- ci/before-install.sh
+++ ci/before-install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_c25aa352ba20_key -iv $encrypted_c25aa352ba20_iv
   -in codesigning.asc.enc -out ci/codesigning.asc -d
   gpg --fast-import ci/codesigning.asc

d5ed8fbfed46553b6f03d02a1181fbec8f667ef4
Barry Hoggard <barry@tristanmedia.com>
2017-05-09 00:05:27 -0400

    typo fix

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,8 +5,7 @@ set -e
 # do snapshot, no release
 if [ "$TRAVIS_BRANCH" = 'development' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_SCALA_VERSION" == "2.11.11" ]; then
   sbt publishSigned
-end
-
+fi
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_SCALA_VERSION" == "2.11.11" ]; then
   sbt publishSigned

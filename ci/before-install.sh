cd1b4b626dc0a298ee5f585695abd5885f94a0fb
Alan Johnson <alan@breakrs.com>
2017-09-20 17:58:00 -0400

    fix failing cp in CI script

--- ci/before-install.sh
+++ ci/before-install.sh
@@ -7,4 +7,5 @@ openssl enc -in codesigning.asc.enc -out codesigning.asc -d -aes256 -k $ENCRYPTI
 gpg --fast-import codesigning.asc
 
 # sbt-sonatype looks in wrong place sometimes for credentials
+mkdir -p ~/.sbt/0.13
 cp project/sonatype.sbt ~/.sbt/0.13/

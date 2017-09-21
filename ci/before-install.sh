ecc12828b9a9057a6a26a5329e042c394ea373c8
Alan Johnson <alan@breakrs.com>
2017-09-21 11:27:51 -0400

    test out not copying sonatype.sbt

--- ci/before-install.sh
+++ ci/before-install.sh
@@ -5,7 +5,3 @@ set -e
 openssl enc -in codesigning.asc.enc -out codesigning.asc -d -aes256 -k $ENCRYPTION_KEY
 
 gpg --fast-import codesigning.asc
-
-# sbt-sonatype looks in wrong place sometimes for credentials
-mkdir -p ~/.sbt/0.13
-cp project/sonatype.sbt ~/.sbt/0.13/
